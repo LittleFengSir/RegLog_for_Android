726f7e062f0881262299955b185ca6779a862f59
小冯Sir <fengsir.hy@foxmail.com>
2024-05-10 10:48:48 +0800

    Improve the registration logic

--- app/src/main/java/com/example/registor_login/Register_Activity.java
+++ app/src/main/java/com/example/registor_login/Register_Activity.java
@@ -1,25 +1,117 @@
 package com.example.registor_login;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Register_Activity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText inputUsername, inputPassword, inputPwdAgain;
+    private String username, password, pwdAgain;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_layout);
         findViewById(R.id.btn_regBackMenu).setOnClickListener(this);
         findViewById(R.id.btn_regMenu).setOnClickListener(this);
+
+        init();
     }
+
+    private void init() {
+        //获取界面控件
+        inputUsername = findViewById(R.id.regInput_username);
+        inputPassword = findViewById(R.id.regInput_password);
+        inputPwdAgain = findViewById(R.id.regInput_pwdAgain);
+
+    }
+
+    private void getData() {
+        username = inputUsername.getText().toString().trim();
+        password = inputPassword.getText().toString().trim();
+        pwdAgain = inputPwdAgain.getText().toString().trim();
+    }
+
+    public static class MyDatabaseHelper extends SQLiteOpenHelper{
+        private static final String DATABASE_NAME = "userInfo.db";
+        private static final int DATABASE_VERSION = 1;
+
+        public MyDatabaseHelper(Context context){
+            super(context,DATABASE_NAME,null,DATABASE_VERSION);
+        }
+        @Override
+        public void onCreate(SQLiteDatabase db){
+            String CREATE_TABLE_MY_DATA = "CREATE TABLE userInfo (" +
+                    "username TEXT NOT NULL," +
+                    "password TEXT NOT NULL" + ")";
+            db.execSQL(CREATE_TABLE_MY_DATA);
+        }
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+            db.execSQL("DROP TABLE IF EXISTS userInfo");
+            onCreate(db);
+        }
+
+    }
+
+    public void showAlertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(Register_Activity.this);
+
+        builder.setTitle("用户信息确认")
+                .setMessage("账号：" + username + "\n" + "密码：" + password);
+        builder.setPositiveButton("确定", (dialog, which) -> {
+            Toast.makeText(Register_Activity.this, "您点击了确认按钮", Toast.LENGTH_SHORT).show();
+            MyDatabaseHelper dbHelper = new MyDatabaseHelper(this);
+            SQLiteDatabase db = dbHelper.getWritableDatabase();
+            ContentValues values = new ContentValues();
+            values.put("username",username);
+            values.put("password",password);
+            db.insert("userInfo",null,values);
+            db.close();
+            dialog.cancel();
+        });
+        builder.setNegativeButton("取消", (dialog, which) -> {
+            Toast.makeText(Register_Activity.this, "您点击了取消按钮", Toast.LENGTH_SHORT).show();
+            dialog.cancel();
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
 
     @Override
     public void onClick(View v) {
-        if(v.getId() == R.id.btn_regMenu){
+        if (v.getId() == R.id.btn_regMenu) {
+            getData();
+            if (TextUtils.isEmpty(username)) {
+                Toast.makeText(Register_Activity.this, "请输入账号", Toast.LENGTH_SHORT).show();
+            } else if (TextUtils.isEmpty(password)) {
+                Toast.makeText(Register_Activity.this, "请输入密码", Toast.LENGTH_SHORT).show();
+            } else if (TextUtils.isEmpty(pwdAgain)) {
+                Toast.makeText(Register_Activity.this, "请再次输入密码", Toast.LENGTH_SHORT).show();
+            } else if (!TextUtils.equals(password, pwdAgain)) {
+                Toast.makeText(Register_Activity.this, "两次输入密码不一致", Toast.LENGTH_SHORT).show();
+            } else {
+                showAlertDialog();
+            }
+
 
         } else if (v.getId() == R.id.btn_regBackMenu) {
             finish();
         }
+
+
     }
 }

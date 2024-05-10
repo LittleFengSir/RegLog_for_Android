00ef1b1b3d71e2d80c8ed6eb6ea92e67b364a12b
小冯Sir <fengsir.hy@foxmail.com>
2024-05-10 10:47:55 +0800

    Improve the login logic

--- app/src/main/java/com/example/registor_login/Login_Activity.java
+++ app/src/main/java/com/example/registor_login/Login_Activity.java
@@ -1,12 +1,17 @@
 package com.example.registor_login;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Login_Activity extends AppCompatActivity implements
         View.OnClickListener{
+    private EditText inputUsername,inputPassword;
+    private String username,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +21,25 @@ public class Login_Activity extends AppCompatActivity implements
         findViewById(R.id.btn_loginMenu).setOnClickListener(this);
     }
 
+    private void Init(){
+        inputUsername = findViewById(R.id.login_InputUsername);
+        inputPassword = findViewById(R.id.login_InputPassword);
+    }
+
+    private void getData(){
+        username = inputUsername.getText().toString().trim();
+        password = inputPassword.getText().toString().trim();
+    }
+
     @Override
     public void onClick(View v) {
         if(v.getId() == R.id.btn_loginMenu){
-
+            getData();
+            if(TextUtils.isEmpty(username)){
+                Toast.makeText(Login_Activity.this,"请输入账号",Toast.LENGTH_SHORT).show();
+            } else if (TextUtils.isEmpty(password)) {
+                Toast.makeText(Login_Activity.this,"请输入密码",Toast.LENGTH_SHORT).show();
+            }
         } else if (v.getId() == R.id.back_loginMenu) {
             finish();
         }

fe796fa96acf53b2c4b2a2f78b702d658a809d3f
小冯Sir <fengsir.hy@foxmail.com>
2024-05-10 15:06:11 +0800

    Basic login logic completed and redirected to the user information interface

--- app/src/main/java/com/example/registor_login/Login_Activity.java
+++ app/src/main/java/com/example/registor_login/Login_Activity.java
@@ -1,5 +1,8 @@
 package com.example.registor_login;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -19,6 +22,7 @@ public class Login_Activity extends AppCompatActivity implements
         setContentView(R.layout.login_layout);
         findViewById(R.id.back_loginMenu).setOnClickListener(this);
         findViewById(R.id.btn_loginMenu).setOnClickListener(this);
+        Init();
     }
 
     private void Init(){
@@ -27,8 +31,8 @@ public class Login_Activity extends AppCompatActivity implements
     }
 
     private void getData(){
-        username = inputUsername.getText().toString().trim();
-        password = inputPassword.getText().toString().trim();
+        username = inputUsername.getText().toString();
+        password = inputPassword.getText().toString();
     }
 
     @Override
@@ -39,6 +43,24 @@ public class Login_Activity extends AppCompatActivity implements
                 Toast.makeText(Login_Activity.this,"请输入账号",Toast.LENGTH_SHORT).show();
             } else if (TextUtils.isEmpty(password)) {
                 Toast.makeText(Login_Activity.this,"请输入密码",Toast.LENGTH_SHORT).show();
+            }else {
+                MyDatabaseHelper dbHelper = new MyDatabaseHelper(this);
+                SQLiteDatabase db = dbHelper.getReadableDatabase();
+                String query = "SELECT * FROM userInfo WHERE username = ? AND password = ?";
+                String[] selectionArgs = {username,password};
+                Cursor cursor = db.rawQuery(query,selectionArgs);
+
+                if(cursor.moveToFirst()){
+                    Toast.makeText(this,"登录成功！",Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(Login_Activity.this, UserInfo_Activity.class);
+                    intent.putExtra("username",username);
+                    intent.putExtra("password",password);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(this,"账号或密码错误！",Toast.LENGTH_SHORT).show();
+                }
+                cursor.close();
+                db.close();
             }
         } else if (v.getId() == R.id.back_loginMenu) {
             finish();
